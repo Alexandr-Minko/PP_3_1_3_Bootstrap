626582b1500cf54f5d5348e4bac6a6363b3b3339
Alexandr-Minko <Minko.Alexandr@gmail.com>
2022-11-08 17:47:48 +0300

    Fix get Roles from DB

--- src/main/java/ru/kata/spring/bootstrap/service/UserServiceImpl.java
+++ src/main/java/ru/kata/spring/bootstrap/service/UserServiceImpl.java
@@ -1,6 +1,5 @@
 package ru.kata.spring.bootstrap.service;
 
-import org.hibernate.Hibernate;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -10,55 +9,53 @@ import ru.kata.spring.bootstrap.dao.RoleRepository;
 import ru.kata.spring.bootstrap.dao.UserRepository;
 import ru.kata.spring.bootstrap.model.User;
 
+import javax.persistence.EntityManager;
 import java.util.List;
 
 
 @Service
-@Transactional(readOnly = true)
+@Transactional
 public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository userRepository;
+    private final EntityManager entityManager;
 
-    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, EntityManager entityManager) {
         this.userRepository = userRepository;
+        this.entityManager = entityManager;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @Override
-    @Transactional(readOnly = false)
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
     @Override
-    @Transactional(readOnly = false)
     public void updateUser(User user) {
         userRepository.save(user);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public User getUser(int id) {
         return userRepository.findById(id).orElse(null);
     }
 
     @Override
-    @Transactional(readOnly = false)
     public void deleteUser(int id) {
         userRepository.deleteById(id);
     }
 
     @Override
+    @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(username);
-        if (user == null) {
-            throw new UsernameNotFoundException("User not found");
-        }
-        Hibernate.initialize(user.getRoles());
-        return user;
+        return entityManager.createQuery("SELECT u FROM User AS u JOIN FETCH u.roles WHERE u.email=:email", User.class)
+                .setParameter("email", username).getSingleResult();
     }
-
 }
